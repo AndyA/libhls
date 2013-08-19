a0da08e2aab54dbbaf9b5de425f42b6a64b3c107
Andy Armstrong <andy@hexten.net>
2013-08-19 13:17:32 +0100

    Allow ordering to be specified.

--- hls_m3u8_formatter.c
+++ hls_m3u8_formatter.c
@@ -108,7 +108,7 @@ static jd_var *format_tag(jd_var *out, jd_var *syn, jd_var *tag, jd_var *val) {
   return out;
 };
 
-static void format_record(jd_var *lb, jd_var *syn, jd_var *rec) {
+static void format_record(jd_var *lb, jd_var *syn, jd_var *rec, jd_var *order) {
   if (!rec) return;
 
   if (rec->type == STRING) {
@@ -122,11 +122,13 @@ static void format_record(jd_var *lb, jd_var *syn, jd_var *rec) {
     jd_delete_ks(rv, "uri", uri);
 
     jd_var *keys = jd_sort(jd_keys(jd_nv(), rv));
+    if (order) keys = jd_append(order, keys);
     size_t count = jd_count(keys);
 
     for (unsigned i = 0; i < count; i++) {
       jd_var *key = jd_get_idx(keys, i);
       jd_var *val = jd_get_key(rv, key, 0);
+      if (!val) continue;
       if (val->type == ARRAY) {
         for (unsigned j = 0; j < jd_count(val); j++)
           format_tag(jd_push(lb, 1), syn, key, jd_get_idx(val, j));
@@ -134,16 +136,17 @@ static void format_record(jd_var *lb, jd_var *syn, jd_var *rec) {
       else {
         format_tag(jd_push(lb, 1), syn, key, val);
       }
+      jd_delete_key(rv, key, NULL);
     }
 
-    if (uri->type != NULL)
+    if (uri->type != VOID)
       jd_assign(jd_push(lb, 1), uri);
   }
 }
 
-static void format_list(jd_var *lb, jd_var *syn, jd_var *list) {
+static void format_list(jd_var *lb, jd_var *syn, jd_var *list, jd_var *order) {
   for (unsigned i = 0; i < jd_count(list); i++)
-    format_record(lb, syn, jd_get_idx(list, i));
+    format_record(lb, syn, jd_get_idx(list, i), order);
 }
 
 jd_var *hls_m3u8_format(jd_var *out, jd_var *m3u8) {
@@ -151,10 +154,11 @@ jd_var *hls_m3u8_format(jd_var *out, jd_var *m3u8) {
     jd_var *syn = hls__get_syntax(m3u8);
     jd_var *lb = jd_nav(4000);
     jd_set_string(jd_push(lb, 1), "#EXTM3U");
+    jd_var *seg_order = jd_set_array_with(jd_nv(), jd_nsv("EXTINF"), NULL);
 
-    format_record(lb, syn, jd_get_ks(m3u8, "meta", 0));
-    format_list(lb, syn, jd_get_ks(m3u8, "vpl", 0));
-    format_list(lb, syn, jd_get_ks(m3u8, "seg", 0));
+    format_record(lb, syn, jd_get_ks(m3u8, "meta", 0), NULL);
+    format_list(lb, syn, jd_get_ks(m3u8, "vpl", 0), NULL);
+    format_list(lb, syn, jd_get_ks(m3u8, "seg", 0), seg_order);
 
     if (jd_get_int(jd_get_ks(m3u8, "closed", 0)))
       jd_set_string(jd_push(lb, 1), "#EXT-X-ENDLIST");
